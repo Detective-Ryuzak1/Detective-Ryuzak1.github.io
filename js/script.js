3ee629041c632c2512ad574eb8f19f42704d9348
Detective-Ryuzak1 <64340847+Detective-Ryuzak1@users.noreply.github.com>
2023-07-23 02:42:16 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -13,3 +13,35 @@ window.onscroll = function () {
     elemento1.style.bottom = posicion * 0.1 + "px";
     elemento2.style.top = posicion * 0.1 + "px";
 }
+
+// Función para copiar el texto del comando al portapapeles
+function copiarTexto(comando) {
+    // Crea un elemento de texto temporal
+    const textoTemp = document.createElement('textarea');
+    textoTemp.value = comando;
+    
+    // Agrega el elemento al DOM para copiar el texto
+    document.body.appendChild(textoTemp);
+    
+    // Selecciona el texto del elemento
+    textoTemp.select();
+    
+    // Copia el texto seleccionado al portapapeles
+    document.execCommand('copy');
+    
+    // Remueve el elemento temporal
+    document.body.removeChild(textoTemp);
+  }
+  
+  // Obtén todos los botones de copiar
+  const botonesCopiar = document.querySelectorAll('.btn-copiar');
+  
+  // Agrega el evento click a cada botón
+  botonesCopiar.forEach(boton => {
+    boton.addEventListener('click', () => {
+      const comando = boton.previousElementSibling.textContent; // Obtiene el texto del comando
+      copiarTexto(comando);
+      alert('Comando copiado al portapapeles: ' + comando);
+    });
+  });
+  
