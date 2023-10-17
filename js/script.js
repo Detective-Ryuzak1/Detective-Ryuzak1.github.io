29a595466e2bac13857a2e4479ce4bfdc213600c
Detective-Ryuzak1 <64340847+Detective-Ryuzak1@users.noreply.github.com>
2023-10-16 19:55:59 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
 document.getElementById("icon_menu").addEventListener("click", mostrar_menu);
-
 function mostrar_menu(){
-
-    document.querySelector(".menu").classList.toggle("mostrar_menu");
-    
+    document.querySelector(".menu").classList.toggle("mostrar_menu");   
 }
+
 
 window.onscroll = function () {
     var posicion = window.pageYOffset || document.documentElement.scrollTop;
@@ -14,30 +12,27 @@ window.onscroll = function () {
     elemento2.style.top = posicion * 0.1 + "px";
 }
 
+
 // Función para ajustar el ancho del input en función de su contenido
 function ajustarAncho(input) {
   input.style.width = 'auto';
   const nuevoAncho = input.scrollWidth + 10; // Agregar un poco de margen
   input.style.width = nuevoAncho + 'px';
 }
-
 // Función para ajustar el ancho del textarea en función de su contenido
 function ajustarAncho(textarea) {
   textarea.style.width = 'auto';
   textarea.style.width = textarea.scrollWidth + 'px';
 }
-
 // Llamar a la función para que los inputs se ajusten al cargarse la página
 const inputs = document.querySelectorAll('.comandos input');
 inputs.forEach((input) => {
   ajustarAncho(input);
 });
-
 function ajustarAncho(textarea) {
   textarea.style.width = 'auto'; // Restablece el ancho a automático
   textarea.style.width = textarea.scrollWidth + 'px'; // Establece el ancho al ancho del contenido
 }
-
 function ajustarAnchoTextarea() {
   const textareas = document.querySelectorAll('.comandos textarea');
   textareas.forEach((textarea) => {
@@ -45,10 +40,8 @@ function ajustarAnchoTextarea() {
     textarea.style.width = textarea.scrollWidth + 'px';
   });
 }
-
 // Llamar a la función después de cargar la página
 window.addEventListener('load', ajustarAnchoTextarea);
-
 // También puedes llamar a esta función cada vez que se introduzca nuevo texto en el textarea
 const textareas = document.querySelectorAll('.comandos textarea');
 textareas.forEach((textarea) => {
@@ -56,13 +49,10 @@ textareas.forEach((textarea) => {
 });
 
 
-
-
 function copiarValorConsola(event) {
   const comandos = event.currentTarget.parentElement;
   const input = comandos.querySelector('.text');
   const valorInput = input.value;
-
   // Copiar el valor del input a la clipboard
   const tempTextArea = document.createElement('textarea');
   tempTextArea.value = valorInput;
@@ -70,18 +60,14 @@ function copiarValorConsola(event) {
   tempTextArea.select();
   document.execCommand('copy');
   document.body.removeChild(tempTextArea);
-
   console.log(valorInput);
-
   // Agregar la clase 'active' al contenedor 'comandos'
   comandos.classList.add('active');
-
   // Eliminar la clase 'active' después de unos segundos
   setTimeout(() => {
     comandos.classList.remove('active');
   }, 2750); // 2000 milisegundos (2 segundos)
 }
-
 // Asociar la función al botón de ambos contenedores
 const botones = document.querySelectorAll('.comandos button');
 botones.forEach((boton) => {
@@ -89,19 +75,14 @@ botones.forEach((boton) => {
 });
 
 
-
-
-
-// Obtén todos los botones con la clase "boton-copiar"
-const botonesCopiar = document.querySelectorAll(".boton-copiar");
-
+// Obtén todos los botones con la clase "boton-copiar-consola"
+const botonesCopiar = document.querySelectorAll(".boton-copiar-consola");
 // Itera a través de los botones y agrega un controlador de eventos a cada uno
 botonesCopiar.forEach((boton) => {
   boton.addEventListener("click", function () {
-    // Encuentra el elemento ".codigo" dentro del contenedor actual
+    // Encuentra el elemento ".codigo-consola" dentro del contenedor actual
     const codigo = this.parentElement.nextElementSibling.querySelector("pre code");
     const codigoTexto = codigo.textContent;
-
     // Crea un elemento de textarea para copiar el código
     const textarea = document.createElement("textarea");
     textarea.value = codigoTexto;
@@ -109,11 +90,9 @@ botonesCopiar.forEach((boton) => {
     textarea.select();
     document.execCommand("copy");
     document.body.removeChild(textarea);
-
     // Cambia el texto del botón a "Copiado"
     this.innerText = "Copied!";
-
-    // Restaura el texto original después de un segundo
+    // Restaura el texto original después de dos segundos
     setTimeout(() => {
       this.innerText = "Copy";
     }, 2000);
